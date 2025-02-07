7d355cdba68b775a5859a39a773f3f3199104bb6
DOCYDD <1662294920@qq.com>
2025-02-07 17:22:41 +0300

    Create login.js

---
+++ pages/login.js
@@ -0,0 +1,17 @@
+// pages/login.js
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" />
+        
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+        
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
